baa03f02f17218512b1659029d02aeb786ffd3eb
Yeneviel Roberts <yeneviel.roberts.7e8@itb.cat>
2025-05-09 08:32:31 +0000

    Base de datos

--- public/index.php
+++ public/index.php
@@ -5,16 +5,91 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$pdo= new PDO ('sqlite:'. __DIR__.'/../Slim/data/database.sqlite');
+$pdo->setAttribute(PDO::AFTER_ERRMODE, PEDO:: ERRMODE_EXCEPTION);
 // Instantiate App
 $app = AppFactory::create();
 
-// Add error middleware
-$app->addErrorMiddleware(true, true, true);
+
 
 // Add routes
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
-    return $response;
+$app->get('/', function (Request $request, Response $response) use ($pdo){
+    $stm=$pdo->query("SELECT * from artists");
+    $artistas=$stm->fetchAll(PDO::FETCH_ASSOC);
+
+    $htmlContent.=`
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\indexCss.css">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <script src="scripts/index.js" defer></script>
+
+</head>
+
+<body>
+    <header id="header">
+        <nav>
+            <h2>El universo musical</h2>
+            <div id="enlancesNav">
+                <a href="busqueda.html"><i class="fas fa-search"></i></a>
+                <a href="index.html">Home</a>
+                <a href="log_in.html">Log in</a>
+                <a href="sobreNosotros.html">About us</a>
+                <a href="soporte.html">Soporte</a>
+                <a href="perfil_usuario.html"><i class="fas fa-user"></i></a>
+            </div>
+        </nav>
+    </header>
+
+    <section id="artitas">
+        <h2>Artistas más escuchados</h2>
+        <div id="lista-artistas">
+        </div>
+        <button id="verMasArtistas" class="arrow-button">
+            <i class="fas fa-chevron-down"></i> <!-- Ícono de flecha -->
+        </button>
+    </section>
+
+    <section id="canciones">
+        <h2>Canciones populares</h2>
+        <div id="lista-canciones">
+        </div>
+        <button id="verMasCanciones" class="arrow-button">
+            <i class="fas fa-chevron-down"></i> <!-- Ícono de flecha -->
+        </button>
+    </section>
+
+    <section id="albumes">
+        <h2>Álbumes recomendados</h2>
+        <div id="lista-albumes">
+        <button id="verMasAlbumes" class="arrow-button">
+            <i class="fas fa-chevron-down"></i> <!-- Ícono de flecha -->
+        </button>
+    </section>
+    <footer id="footer">
+        <div>
+            <p>© 2025 El universo musical</p>
+            <div class="social-icons">
+                <a href="#" target="_blank" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                <a href="#" target="_blank" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" target="_blank" aria-label="YouTube"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
+    
+    
+    
+    `;
+   
 });
 
 $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
@@ -24,3 +99,5 @@ $app->get('/hello/{name}', function (Request $request, Response $response, $args
 });
 
 $app->run();
+
+?>
